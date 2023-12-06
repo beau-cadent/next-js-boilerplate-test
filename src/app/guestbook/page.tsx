0ff7f35368c207f30c69ebb040c7275c0e3cceba
Beau Jorgensen <beau@Beaus-Laptop.gateway>
2023-12-06 13:56:46 +1000

    refactor: remove sponsored link
    asgedrgsdvbserthbaerga rg aerg awersgasedrg asedrbsedtrhgae
    
    ABC-123

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import Image from 'next/image';
 
 import { DeleteGuestbookEntry } from '@/components/DeleteGuestbookEntry';
 import { EditableGuestbookEntry } from '@/components/EditableGuestbookEntry';
@@ -33,26 +32,6 @@ const Guestbook = async () => {
           </div>
         ))}
       </div>
-
-      <div className="mt-2 text-center text-sm">
-        Database powered by{' '}
-        <a
-          href="https://turso.tech/?utm_source=nextjsstarterbp"
-          target="_blank"
-        >
-          Turso
-        </a>
-      </div>
-
-      <a href="https://turso.tech/?utm_source=nextjsstarterbp">
-        <Image
-          className="mx-auto mt-2"
-          src="/assets/images/turso.png"
-          alt="SQLite Developer Experience"
-          width={65}
-          height={56}
-        />
-      </a>
     </Main>
   );
 };
