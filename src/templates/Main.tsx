febdaaa4e4cdbd0106049b992b64d77578226bd8
Beau Jorgensen <beau@Beaus-Laptop.gateway>
2023-12-06 13:48:19 +1000

    fix: remove footer

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -101,16 +101,7 @@ const Main = (props: IMainProps) => (
 
       <main className="content py-5 text-xl">{props.children}</main>
 
-      <footer className="border-t border-gray-300 py-8 text-center text-sm">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}. Made with{' '}
-        <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>.
-        {/*
-         * PLEASE READ THIS SECTION
-         * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-         * The link doesn't need to appear on every pages, one link on one page is enough.
-         * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-         */}
-      </footer>
+      <footer className="border-t border-gray-300 py-8 text-center text-sm" />
     </div>
   </div>
 );
